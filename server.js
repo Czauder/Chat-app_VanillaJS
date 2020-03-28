0cadffd94140cbcad4fdaf2e05e61e70d9a30bc2
Przemyslaw Czauderna <pczauderna@prescientco.com>
2020-03-28 15:33:19 +0100

    extend user functionality per get room users and get current user

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const socketio = require('socket.io');
 const formatMessage = require('./utils/messages')
 const {
     userJoin,
-    getCurrentUser
+    getCurrentUser,
+    userLeave,
+    getRoomUsers
 } = require('./utils/users')
 
 const app = express();
@@ -24,23 +26,32 @@ io.on('connection', socket => {
         username,
         room
     }) => {
-
+        const user = userJoin(socket.id, username,
+            room)
+        socket.join(user.room)
 
         // Welcome current user
         socket.emit('message', formatMessage(botName, 'Welcome to chat'))
 
         // Broadcast when a user connects
-        socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
     })
 
     // Listen for chatMessage
     socket.on('chatMessage', msg => {
-        io.emit('message', formatMessage('USER', msg))
+        const user = getCurrentUser(socket.id);
+
+        io.to(user.room).emit('message', formatMessage(user.username, msg))
     })
 
     // Runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', formatMessage(botName, 'A user has left the chat'));
+        const user = userLeave(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
+        }
+
     })
 })
 
