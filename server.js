bbcf0d17d2b725a2a9025a0040b1917326941294
Przemyslaw Czauderna <pczauderna@prescientco.com>
2020-03-28 15:53:39 +0100

    clean-up code

--- server.js
+++ server.js
@@ -14,12 +14,10 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-// Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 const botName = 'ChatCord Bot';
 
-// Run when client connects
 io.on('connection', socket => {
     socket.on('joinRoom', ({
         username,
@@ -29,10 +27,8 @@ io.on('connection', socket => {
 
         socket.join(user.room);
 
-        // Welcome current user
         socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
 
-        // Broadcast when a user connects
         socket.broadcast
             .to(user.room)
             .emit(
@@ -40,21 +36,18 @@ io.on('connection', socket => {
                 formatMessage(botName, `${user.username} has joined the chat`)
             );
 
-        // Send users and room info
         io.to(user.room).emit('roomUsers', {
             room: user.room,
             users: getRoomUsers(user.room)
         });
     });
 
-    // Listen for chatMessage
     socket.on('chatMessage', msg => {
         const user = getCurrentUser(socket.id);
 
         io.to(user.room).emit('message', formatMessage(user.username, msg));
     });
 
-    // Runs when client disconnects
     socket.on('disconnect', () => {
         const user = userLeave(socket.id);
 
@@ -64,7 +57,6 @@ io.on('connection', socket => {
                 formatMessage(botName, `${user.username} has left the chat`)
             );
 
-            // Send users and room info
             io.to(user.room).emit('roomUsers', {
                 room: user.room,
                 users: getRoomUsers(user.room)
--- utils/users.js
+++ utils/users.js
@@ -1,6 +1,5 @@
   const users = [];
 
-  // Join user to chat
   function userJoin(id, username, room) {
       const user = {
           id,
@@ -13,12 +12,10 @@
       return user;
   }
 
-  // Get current user
   function getCurrentUser(id) {
       return users.find(user => user.id === id);
   }
 
-  //   User leaves chat
   function userLeave(id) {
       const index = users.findIndex(user => user.id === id);
 
@@ -27,7 +24,6 @@
       }
   }
 
-  // Get room users
   function getRoomUsers(room) {
       return users.filter(user => user.room === room);
   }
