7d8763224850c48a702b786f30bb0e410ce6fb79
Przemyslaw Czauderna <pczauderna@prescientco.com>
2020-03-28 14:35:54 +0100

    added qs library for parser querystring

--- server.js
+++ server.js
@@ -12,25 +12,29 @@ const io = socketio(server)
 // Set static folder 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = "ChatCord Bot"
+
 // Run when client connect
 io.on('connection', socket => {
+    socket.on('joinRoom', ({
+        username,
+        room
+    }) => {
+        // Welcome current user
+        socket.emit('message', formatMessage(botName, 'Welcome to chat'))
 
-    const botName = "ChatCord Bot"
-
-    // Welcome current user
-    socket.emit('message', formatMessage(botName, 'Welcome to chat'))
-
-    // Broadcast when a user connects
-    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
-
-    // Runs when client disconnects
-    socket.on('disconnect', () => {
-        io.emit('message', formatMessage(botName, 'A user has left the chat'));
+        // Broadcast when a user connects
+        socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
     })
 
     // Listen for chatMessage
     socket.on('chatMessage', msg => {
         io.emit('message', formatMessage('USER', msg))
+    })
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
     })
 })
 
