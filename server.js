6142e0f4303ce5d960b8bd9cfa8519a77755be8a
Przemyslaw Czauderna <pczauderna@prescientco.com>
2020-03-28 14:24:55 +0100

    Created model for passing messages

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const path = require('path')
 const http = require('http');
 const express = require('express');
-const socketio = require('socket.io')
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages')
 
 const app = express();
 
@@ -13,21 +14,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Run when client connect
 io.on('connection', socket => {
-   
+
+    const botName = "ChatCord Bot"
+
     // Welcome current user
-    socket.emit('message', "Welcome to chat")
+    socket.emit('message', formatMessage(botName, 'Welcome to chat'))
 
     // Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined the chat');
-    
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
+
     // Runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
     })
 
     // Listen for chatMessage
     socket.on('chatMessage', msg => {
-        io.emit('message', msg)
+        io.emit('message', formatMessage('USER', msg))
     })
 })
 
